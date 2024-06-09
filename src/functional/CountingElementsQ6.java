0319a14c562a797b172e173320abb6244e5b5305
Raihan92 <raihancse1992@gmail.com>
2024-06-09 17:48:29 -0400

    Flattering nested list using stream

---
+++ src/functional/CountingElementsQ6.java
@@ -0,0 +1,17 @@
+package functional;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountingElementsQ6 {
+    public static void main(String[] args) {
+        /*
+        Question: Given a list of strings, count how many strings have a length greater than 3 using streams.
+         */
+        List<String> words = Arrays.asList("Java", "is", "fun", "and", "powerful");
+        Long count = words.stream()
+                .filter(word -> word.length() > 3)
+                .count();
+        System.out.println(count);
+    }
+}
