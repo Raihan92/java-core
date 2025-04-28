eabfd289d2e14e97f78b5feffa1ed9c0d34fe19c
Raihan92 <raihancse1992@gmail.com>
2025-04-28 12:25:50 -0500

    added Q10

--- src/functional/ReverseTheNumberListQ10.java
+++ src/functional/ReverseTheNumberListQ10.java
@@ -10,9 +10,11 @@ public class ReverseTheNumberListQ10 {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(Arrays.asList(2,1,3,4,0,6,10,7,8,5,9));
         List<Integer> newNums = new ArrayList<>();
-        nums.stream().collect(Collectors.collectingAndThen(Collectors.toList(),list-> {
-                            Collections.reverse(list);return list.stream();
-                        })).forEach(num -> newNums.add(num));
+        newNums = nums.stream()
+                      .collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
+                          Collections.reverse(list);
+                          return list;
+                      }));
         System.out.println(newNums);
     }
 }
